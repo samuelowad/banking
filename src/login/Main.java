872c9633a7f32e1b070a9d46cde40b72fa8f6ccf
samuelowad <samuelowolabi330@gmail.com>
2020-05-01 21:56:26 +0100

    lets go

---
+++ src/login/Main.java
@@ -0,0 +1,25 @@
+package login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("design/design.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
